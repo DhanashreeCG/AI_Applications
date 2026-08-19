4a758a5e121d01090e8e2eeb1d4ed40e1877f09e
Shubham Ingole <shubhamingole@creativegalileo.com>
2026-08-19 10:44:45 +0530

    Fixed s3 upload of flashcard and size of image on flashcard

--- src/modules/worksheets/services/worksheet-asset.service.ts
+++ src/modules/worksheets/services/worksheet-asset.service.ts
@@ -4,7 +4,10 @@ import { EventEmitter2 } from '@nestjs/event-emitter';
 import { randomUUID } from 'node:crypto';
 import { SearchService } from '../../search/search.service';
 import type { SearchAssetsResponse } from '../../search/interfaces/search-result.interface';
-import { S3StorageService } from '../../storage/s3-storage.service';
+import {
+  S3StorageService,
+  sanitizeUploadFilename,
+} from '../../storage/s3-storage.service';
 import { PrismaService } from '../../database/prisma.service';
 import {
   PIPELINE_STAGES,
@@ -419,7 +422,10 @@ export class WorksheetAssetService {
     await this.s3StorageService.uploadFile(file.buffer, {
       key,
       contentType,
-      metadata: { worksheetId, originalname: file.originalname || uploadId },
+      metadata: {
+        worksheetId,
+        originalname: sanitizeUploadFilename(file.originalname, uploadId),
+      },
     });
     return {
       key,
